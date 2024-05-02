94ae820184c073764ed18060c79ace18e9087c3d
Märt Mattias Alas <marttest9@gmail.com>
2024-05-02 14:10:10 +0300

    Added navbar to website

--- js/script.js
+++
@@ -1,9 +0,0 @@
-if ('geolocation' in navigator) {
-  navigator.geolocation.getCurrentPosition((position) => {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-    console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
-  });
-} else {
-  console.log('Geolocation is not supported by this browser.');
-}
