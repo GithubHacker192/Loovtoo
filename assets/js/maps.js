87c339c062ddf55d23317eb9eb1f3ac2c97465ae
Märt Mattias Alas <marttest9@gmail.com>
2024-05-15 07:42:06 +0300

    Removing marker

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -18,7 +18,7 @@ function myMap() {
       longitude_element.innerHTML = longitude;
       let location = google.maps.LatLng(latitude, longitude);
       map.setCenter(location)
-      createMarker(location, map);
+      // createMarker(location, map);
       console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
     });
   } else {
